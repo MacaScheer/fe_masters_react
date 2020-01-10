b73546867d414ea5e1cf4a1bf7c6f4064a4a9fa4
Michael_Scheer <macas.scheer@gmail.com>
2020-01-10 15:09:50 -0800

    passed in destructred props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    React.createElement("h1", {}, "Adopt Me!")
-  );
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Benny",
+      animal: "Dog",
+      breed: "Staffordshire Terrier"
+    }),
+    React.createElement(Pet, {
+      name: "Lulu",
+      animal: "Dog",
+      breed: "Labrador"
+    }),
+    React.createElement(Pet, {
+      name: "Squeeks",
+      animal: "Mouse",
+      breed: "lil white paws"
+    })
+  ]);
 };
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
