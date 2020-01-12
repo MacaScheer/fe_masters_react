26e556cddb930ef062cae26f768fd7a59249777d
Michael_Scheer <macas.scheer@gmail.com>
2020-01-11 18:59:07 -0800

    search component

---
+++ src/SearchParams.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const SearchParams = () => {
+  const location = "Seattle, WA";
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input id="location" value={location} placeholder="Location" />
+        </label>
+      </form>
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default SearchParams;
