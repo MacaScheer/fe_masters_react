a4870d31d55e127e6654bf803f9f9056f9d54b7f
Michael_Scheer <macas.scheer@gmail.com>
2020-01-12 12:11:09 -0800

    fixed variable naming and parenthesis

--- src/useDropDown.js
+++ src/useDropDown.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 const useDropDown = (label, defaultState, options) => {
-  const [state, useState] = useState(defaultState);
+  const [state, setState] = useState(defaultState);
   const id = `use-dropdown-${label.replace(" ", "").toLowerCase()}`;
   const DropDown = () => (
     <label htmlFor={id}>
@@ -11,12 +11,17 @@ const useDropDown = (label, defaultState, options) => {
         onChange={e => setState(e.target.value)}
         onBlur={e => setState(e.target.value)}
         disabled={options.length === 0}
-          >
-              <option>All</option>
-              {options.map(item) => (
-              <option ket={item} value={item}>{item}</option>
-              )}
+      >
+        <option>All</option>
+        {options.map(item => (
+          <option key={item} value={item}>
+            {item}
+          </option>
+        ))}
       </select>
     </label>
   );
+  return [state, DropDown, setState];
 };
+
+export default useDropDown;
